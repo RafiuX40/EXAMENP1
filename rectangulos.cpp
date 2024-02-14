f32f902bd1353d6be4463e8549fdea1868aed13d
Rafael Ramos <22090@portalcedi.edu.mx>
2024-02-14 13:56:42 -0600

    Creacion de funciones y especificar acciones

--- rectangulos.cpp
+++ rectangulos.cpp
@@ -1,30 +1,106 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-class Rectangulo{
+class Rectangulo
+{
 
-    private:
-        int ancho;
-        int altura;
-    public:
-        int esquinaX;
-        int esquinay;
+private:
+    int ancho;
+    int altura;
+
+public:
+    int esquinaX;
+    int esquinay;
 
     Rectangulo();
     Rectangulo(int x, int y);
     Rectangulo(int x, int y, int w, int h);
 
-    void getAncho();
+    int getAncho();
     void setAncho(int w);
-    void getAltura();
+    int getAltura();
     void setAltura(int h);
-    void getArea();
-    void intersecta(Rectangulo rectangulo);   
+    int getArea();
+    void intersecta(Rectangulo rectangulo);
 };
 
-int main(){
+int main()
+{
+}
+
+Rectangulo::Rectangulo()
+{
+
+    ancho = 0;
+    altura = 0;
+    esquinaX = 0;
+    esquinay = 0;
+}
+
+Rectangulo::Rectangulo(int x, int y)
+{
+
+    ancho = 0;
+    altura = 0;
+    esquinaX = x;
+    esquinay = y;
+}
+
+Rectangulo::Rectangulo(int x, int y, int w, int h)
+{
+
+    if (w > 0)
+    {
+        if (h > 0)
+        {
+            ancho = w;
+            altura = h;
+            esquinaX = x;
+            esquinay = y;
+        }
+    }
+    else
+    {
+        ancho = 0;
+        altura = 0;
+    }
+}
+
+int Rectangulo::getAncho(){
+
+    return ancho;
 
 }
 
+void Rectangulo::setAncho(int w){
+
+    ancho = w;
+
+}
+
+int Rectangulo::getAltura(){
+
+    return altura;
+
+}
+
+void Rectangulo::setAltura(int h){
+
+    altura = h;
+
+}
+
+int Rectangulo::getArea(){
+
+    int area;
+    area = altura*ancho; 
+    return area;
+
+}
+
+void Rectangulo::intersecta(Rectangulo rect){
+
+    
+
+}
